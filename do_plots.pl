f38a4dca9be6a916f078ed6fd973e09d8e4de268
chamlin <Chris.Hamlin@marklogic.com>
2017-06-29 10:31:42 -0400

    nada

--- do_plots.pl
+++ do_plots.pl
@@ -30,7 +30,9 @@ for (my $i = 1; $i <= $#columns; $i++) {
     my $index = $i + 1;
     my $col = $columns[$i];
     my $title = $col;
-if ($title =~ /^Forest/) { next }
+if ($title =~ /^Forest/) { 
+    next
+}
     # underbar does sub a la TeX
     $title =~ s/_/\\_/g;
     my $filename = "$col.plot";
